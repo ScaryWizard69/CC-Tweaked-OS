7f3335e82b573c757670001cb06a8b4b391c1d1e
ScaryWizard69 <IvoBickford-smith@mail.com>
2025-07-05 10:54:40 +0000

    changed programs page

--- OS/programs.lua
+++ OS/programs.lua
@@ -1,10 +1,19 @@
-local menuItems = {
-    { name = "chat", action = function() shell.run("OS/programs/chat.lua") end},
-    { name = "sendFile", action = function() shell.run("OS/programs/sendfile.lua") end},
-    { name = "recvFile", action = function() shell.run("OS/programs/recvfile.lua") end},
-    { name = "Back", action = function() shell.run("OS/Menu.lua") end},
+local menuPages = {
+    {
+        { name = "chat", action = function() shell.run("OS/programs/chat.lua") end },
+        { name = "sendFile", action = function() shell.run("OS/programs/sendfile.lua") end },
+        { name = "recvFile", action = function() shell.run("OS/programs/recvfile.lua") end },
+        { name = "Back", action = function() shell.run("OS/Menu.lua") end },
+    },
+    {
+        { name = "Notepad", action = function() print("Notepad coming soon") end },
+        { name = "Calculator", action = function() print("Calculator coming soon") end },
+        { name = "Settings", action = function() print("Settings coming soon") end },
+        { name = "Back", action = function() shell.run("OS/Menu.lua") end },
+    }
 }
 
+local currentPage = 1
 local selection = 1
 
 local function drawMenu()
@@ -15,10 +24,11 @@ local function drawMenu()
     term.setCursorPos(1,1)
     term.write("IOS")
     term.setCursorPos(1,2)
-    term.write("V1.0.0")
+    term.write("V1.0.0 Page " .. currentPage .. "/" .. #menuPages)
 
-    -- centered menu
+    local menuItems = menuPages[currentPage]
     local startY = math.floor((h - #menuItems) / 2)
+
     for i, item in ipairs(menuItems) do
         local prefix = (i == selection) and "-> " or "   "
         local text = prefix .. item.name
@@ -38,14 +48,22 @@ while true do
 
     if key == keys.up then
         selection = selection - 1
-        if selection < 1 then selection = #menuItems end
+        if selection < 1 then selection = #menuPages[currentPage] end
     elseif key == keys.down then
         selection = selection + 1
-        if selection > #menuItems then selection = 1 end
+        if selection > #menuPages[currentPage] then selection = 1 end
+    elseif key == keys.left then
+        currentPage = currentPage - 1
+        if currentPage < 1 then currentPage = #menuPages end
+        selection = 1
+    elseif key == keys.right then
+        currentPage = currentPage + 1
+        if currentPage > #menuPages then currentPage = 1 end
+        selection = 1
     elseif key == keys.enter then
         term.clear()
         term.setCursorPos(1,1)
-        menuItems[selection].action()
-        break  -- remove this break if you want to return to menu after running action
+        menuPages[currentPage][selection].action()
+        break  -- remove if you want to return to menu after action
     end
 end
